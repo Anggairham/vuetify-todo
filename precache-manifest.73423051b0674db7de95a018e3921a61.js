c92d0adcb9e2f248ab604be26dceedba5e94b425
Angga Irham Stianto <anggairham08@gmail.com>
2022-01-09 12:19:54 +0700

    deploy

---
+++ precache-manifest.73423051b0674db7de95a018e3921a61.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af94cade56542b4755b",
+    "url": "/vuetify-todo/css/chunk-vendors.0ec53cca.css"
+  },
+  {
+    "revision": "ba197625ced96cc8c6f0",
+    "url": "/vuetify-todo/css/todo.258ba9ba.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/vuetify-todo/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "ddb25b76228d25a168ea7e27b044d2c7",
+    "url": "/vuetify-todo/index.html"
+  },
+  {
+    "revision": "7e9f9eca402b0870f565",
+    "url": "/vuetify-todo/js/about.81f416f8.js"
+  },
+  {
+    "revision": "3afc98184ab488d6e0c5",
+    "url": "/vuetify-todo/js/app.1a32bfab.js"
+  },
+  {
+    "revision": "2af94cade56542b4755b",
+    "url": "/vuetify-todo/js/chunk-vendors.60647d99.js"
+  },
+  {
+    "revision": "ba197625ced96cc8c6f0",
+    "url": "/vuetify-todo/js/todo.14b2948e.js"
+  },
+  {
+    "revision": "6fc00b4ee4bd18b43141c58e53418e69",
+    "url": "/vuetify-todo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetify-todo/robots.txt"
+  }
+]);
